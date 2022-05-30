5e9713798093ed713ca355324549c16961821514
Sycamore <64587684+Sycamore0@users.noreply.github.com>
2022-05-31 00:46:45 +0800

    (test)Create Boba packetIds Extraction Tool.
    I am not sure...

---
+++ getcmdids_boba_zh-cn_dev.js
@@ -0,0 +1,49 @@
+/*
+
+I never debug this.
+
+Maybe it cannot work well.
+
+Based on Boba Rewritten.
+
+*/
+
+function getCmdids() {
+
+  const fs = require('fs');
+
+  const protoPath = './proto/';
+
+  console.log('找到proto文件。请稍后。');
+
+  const protoFiles = fs.readdirSync(protoPath);
+
+  let protoStr = '';
+
+  protoFiles.forEach(file => {
+
+    const content = fs.readFileSync(protoPath + file, 'utf-8');
+
+    const cmdId = content.match(/CMD_ID = (\d+);/);
+
+    if (cmdId) {
+
+      protoStr += `   "${file.replace('.proto', '')}": "${cmdId[1]}",\n`;
+
+    }
+
+  })
+
+  fs.writeFileSync('./packetIds-boba.json', '{\n   ','utf-8');
+
+  fs.appendFileSync('./packetIds-boba.json', protoStr);
+
+  const appendFileContent = '}';
+
+  fs.appendFileSync('./packetIds-boba.json', appendFileContent, 'utf-8');
+
+}
+
+getCmdids();
+
+console.log('(test)操作成功完成。请查看./packetIds-boba.json。');
