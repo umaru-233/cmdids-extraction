d7359305612b36486ef1763abfeffec689683697
JuliusP <93566768+JuliusPtolemy@users.noreply.github.com>
2022-05-31 17:56:03 +0800

    Update start_zh-cn.bat

--- start_zh-cn.bat
+++ start_zh-cn.bat
@@ -1,17 +1,17 @@
 @echo off
-title cmdid��ȡ���� 1.0.2
-echo. �汾��1.0.2
-echo. �����ߣ�umaru
-echo. �������ڣ�May 22, 2022
-echo. ��ӭʹ��cmdid��ȡ���ߡ�
-echo. ���棺�뽫��������ȡ��proto�ļ�����./proto/Ŀ¼�ڣ���������޷����С�
-echo. �ڿ�ʼǰ����ȷ�����Ѱ�װnodejs�����δ��װ����ǰ���˴����ء�
+title cmdid提取工具 1.0.2
+echo. 版本：1.0.2
+echo. 开发者：umaru
+echo. 构建日期：May 22, 2022
+echo. 欢迎使用cmdid提取工具。
+echo. 警告：请将所有欲提取的proto文件放在./proto/目录内，否则程序将无法运行。
+echo. 在开始前，请确定你已安装nodejs。如果未安装，请前往此处下载。
 echo. https://nodejs.org/
 goto menu
 :menu
-echo.[1] Pancake ��ʽ (packetIds.json) 
-echo.[2] Grasscutter ��ʽ (PacketOpcodes.java)
-set /p choose_format=�밴1��2 
+echo.[1] Pancake 格式 (packetIds.json)
+echo.[2] Grasscutter 格式 (PacketOpcodes.java)
+set /p choose_format=请按1或2 
 if %choose_format%==1 goto :pancake
 if %choose_format%==2 goto :grasscutter
 :pancake
