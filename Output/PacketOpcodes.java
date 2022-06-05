46d1a93f46c84634dd212488002d39d2946c6e31
Sycamore <64587684+Sycamore0@users.noreply.github.com>
2022-06-06 07:13:36 +0800

    uh

---
+++ Output/PacketOpcodes.java
@@ -0,0 +1,15 @@
+package emu.grasscutter.net.packet;
+
+import java.util.HashSet;
+
+public class PacketOpcodes {
+    public static final int NONE = 0;
+
+public static final int FuckUmiHoMoReq = 114514;
+public static final int FuckUmiHoMoRsp = 1919810;
+
+public static final HashSet<Integer> BANNED_PACKETS = new HashSet<Integer>() {{
+    add(PacketOpcodes.WindSeedClientNotify);
+    add(PacketOpcodes.PlayerLuaShellNotify);
+    }};
+}
